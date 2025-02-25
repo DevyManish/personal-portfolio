ad2fd8e7736b9d3b476516f51a24b81f67facfca
DevyManish <121728309+DevyManish@users.noreply.github.com>
2025-02-26 00:58:44 +0530

    add header for mobile screens

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,33 @@
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
 import Marquee from "@/components/Marquee";
+import MobHeader from "@/components/MobHeader";
+import { items } from "@/lib/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-full bg-black overflow-hidden">
-      {/* Background pattern */}
+      {/*pattern */}
       <div className="absolute inset-0 h-full w-full 
         bg-[linear-gradient(to_right,#73737310_1px,transparent_1px),linear-gradient(to_bottom,#73737310_1px,transparent_1px)] 
         bg-[size:20px_20px] 
         [mask-image:radial-gradient(ellipse_at_center,black_10%,transparent_90%)]">
       </div>
 
-      {/* Content container */}
       <div className="relative z-10 h-full">
-        <Header />
+        <div className="hidden md:block">
+          <Header />
+        </div>
+        <div className="block md:hidden">
+
+          <MobHeader
+            items={items}
+            panelHeight={68}
+            baseItemSize={50}
+            magnification={70}
+          />
+        </div>
         <div className="container mx-auto px-4 md:py-12">
           <div className="flex flex-col min-h-[calc(100vh-80px)] items-center justify-center">
             <HomeSection />
--- src/lib/data.js
+++ src/lib/data.js
@@ -23,4 +23,19 @@ export const links = [
         name: "Contact",
         hash: "#contact",
     },
-] 
+]
+
+import { IoHomeOutline } from "react-icons/io5";
+import { GoProjectSymlink } from "react-icons/go";
+
+
+export const items = [
+    {
+        icon: <IoHomeOutline
+            size={18} color={'white'} />, label: 'Home'
+    },
+    { icon: <GoProjectSymlink size={18} color={'white'} />, label: 'Archive' },
+    { icon: <IoHomeOutline size={18} color={'white'} />, label: 'Profile' },
+    { icon: <IoHomeOutline size={18} color={'white'} />, label: 'Settings' },
+];
+
