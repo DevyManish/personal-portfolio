2638349853c720c994f6ad374725df16d73ea47c
DevyManish <121728309+DevyManish@users.noreply.github.com>
2025-02-28 02:57:51 +0530

    disable eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-nextConfig.images = {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'assets.aceternity.com',
-        }
-    ]
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'assets.aceternity.com',
+            }
+        ]
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
 };
+
 export default nextConfig;
