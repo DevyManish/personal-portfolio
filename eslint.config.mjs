c6f97083126beb2c3269a1987350804e60d71573
DevyManish <121728309+DevyManish@users.noreply.github.com>
2025-02-28 02:29:19 +0530

    fix: eslint build issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  {
+    plugins: {
+      "@next/next": next,
+    },
+    rules: {
+      "@next/next/no-html-link-for-pages": "error",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+    },
+    rules: {
+      // Add TypeScript-specific rules here
+    },
+  },
+];
